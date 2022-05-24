e8fbe1c3cc5edafbc32113ccb6491bdcc2c7504c
SergioDreher <sergio.leo.dreher@gmail.com>
2022-05-23 21:45:36 -0300

    comenzando con "experiencia"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoProgramaComponent } from './components/logo-programa/logo-programa.component';
 import { RedessocComponent } from './components/redessoc/redessoc.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoProgramaComponent,
     RedessocComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule
