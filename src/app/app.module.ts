91a2ab2eb563f71bcb2874d674cd090477ad193e
SergioDreher <sergio.leo.dreher@gmail.com>
2022-05-27 00:33:22 -0300

    Unificando header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LogoProgramaComponent } from './components/logo-programa/logo-programa.component';
-import { RedessocComponent } from './components/redessoc/redessoc.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -12,20 +9,21 @@ import { EducacionFormalComponent } from './components/educacion-formal/educacio
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ProgresoComponent } from './components/progreso/progreso.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Header1Component } from './components/header1/header1.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    LogoProgramaComponent,
-    RedessocComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionFormalComponent,
     ProgresoComponent,
-    ProyectoComponent
+    ProyectoComponent,
+    FooterComponent,
+    Header1Component
   ],
   imports: [
     BrowserModule,
--- src/app/components/header1/header1.component.spec.ts
+++ src/app/components/header1/header1.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LogoProgramaComponent } from './logo-programa.component';
+import { Header1Component } from './header1.component';
 
-describe('LogoProgramaComponent', () => {
-  let component: LogoProgramaComponent;
-  let fixture: ComponentFixture<LogoProgramaComponent>;
+describe('Header1Component', () => {
+  let component: Header1Component;
+  let fixture: ComponentFixture<Header1Component>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LogoProgramaComponent ]
+      declarations: [ Header1Component ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LogoProgramaComponent);
+    fixture = TestBed.createComponent(Header1Component);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
